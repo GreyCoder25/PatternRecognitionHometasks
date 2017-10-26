dd79aab3b8cf712866a0c82a2e77cc5a39da43d6
serhii <serhiylatyuk55@gmail.com>
2017-10-26 12:58:02 +0300

    almost finish writing estimation of probabilities of letters and pairs of letters

--- estimating_probabilities.py
+++ estimating_probabilities.py
@@ -1,5 +1,39 @@
 import numpy as np
 
 
-def estimate_probabilities(file_ua, file_ru):
+def estimate_probabilities(file_name, letters):
 
+    N = len(letters)
+
+    p = {}
+    for l in letters:
+        p[l] = 0
+
+    p_pair = {}
+    for l1 in letters:
+        for l2 in letters:
+            p_pair[l1 + l2] = 0
+
+    with open(file_name, 'r') as f:
+        total = 0
+        pair_total = 0
+        ch = f.read(1)
+        ch = ch.lower()
+        prev_ch = ''
+        while ch:
+            if ch in letters:
+                total += 1
+                p[ch] += 1
+                if prev_ch in letters:
+                    p_pair[prev_ch + ch] += 1
+                    pair_total += 1
+            prev_ch = ch
+            ch = f.read(1)
+
+        for ch in p:
+            p[ch] /= total
+
+        for pair in p_pair:
+            p_pair[pair] /= pair_total
+
+    
